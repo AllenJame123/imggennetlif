6ed619b9a8f21fccf45462977edf3fb7c39db286
AllenJame123 <allenjame1234@.com>
2025-08-05 16:05:55 +0500

    Allow 'any' for dynamic fabric import in Editor.tsx; unblock Netlify build

--- src/components/text-on-photo/Editor.tsx
+++ src/components/text-on-photo/Editor.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import TextStyleControls from "./TextStyleControls";
